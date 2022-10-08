0086a3c6c87d217e9aa3be4c049a7e79b1953ab8
nastyafallen <nastyafallen@rambler.ru>
2022-10-08 21:58:21 +0300

    Initial commit

---
+++ src/main/java/ru/hogwarts/school/Homework32Application.java
@@ -0,0 +1,13 @@
+package ru.hogwarts.school;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Homework32Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Homework32Application.class, args);
+    }
+
+}
